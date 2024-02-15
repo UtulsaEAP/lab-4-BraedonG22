0dd46e18b93db50cd54d1d3e5b6ebb711e790749
BraedonG22 <bjg8631@utulsa.edu>
2024-02-15 10:35:49 -0600

    Did it

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,15 +1,19 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
-
+Name: Braedon Gehring
+Lab Time: Friday, 3:00 pm
 """
 
 
 def reverse_binary():
-    user_num = int(input())
+    user_num = int(input("Please provide a positive integer: "))
     # YOUR CODE HERE
+    x = user_num
+    while x > 0 :
+        remainder = x % 2
+        print(f'{str(remainder)}' , end = '')
+        x = int(x / 2)
 
 if __name__ == "__main__":
     reverse_binary()
--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,12 +1,12 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Braedon Gehring
+Lab Time: Friday, 3:00 pm
 """
 
 def password_mod():
-    word = input()
+    word = input('mypassword')
     password = ''
     # Type your code here.
 
