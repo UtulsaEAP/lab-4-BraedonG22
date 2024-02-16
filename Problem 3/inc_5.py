aecd8f9d9fb44834295b11d75b0e3f322529d04a
BraedonG22 <bjg8631@utulsa.edu>
2024-02-16 10:19:52 -0600

    Did Part 3

--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -1,15 +1,22 @@
 """
 Complete the following python code to print all numbers between the two values incrementing by 5 from the initial value to the final value. The initial value and final value are entered by the user. If the final value is less than the initial value, print "Second integer can't be less than the first.
 
-Name:
-Lab Time:
+Name: Braedon Gehring
+Lab Time: Friday, 3:00 pm
 """
 
 def inc_5():
     # Write your code here
-    
+    num1 = int(input("Please provide a multiple of 5: "))
+    num2 = int(input("Please provide another multiple of 5 that is greater than the first: "))
+    sequence = range(num1 , num2 + 5 , 5)
+    if num1>num2 :
+        print("Second number must be greater than the first.")
+    else :
+        for num in sequence :
+            print(f'{str(num)}' , end = ' ' )
+        
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     inc_5()
