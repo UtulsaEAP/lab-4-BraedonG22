859f9584373c1d14d01f5e20bb1291a3c478e79e
BraedonG22 <bjg8631@utulsa.edu>
2024-02-16 12:34:15 -0600

    Did Part 6

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -1,12 +1,48 @@
 """
 Complete the following python code to take in a list of values from the user and then normalize them
 
-Name:
-Lab Time:
+Name: Braedon Gehring
+Lab Time: Friday, 3:00 pm
 """
 
 def norm():
     # Write your code here
-
+    task = str("Provide 5 integer values")
+    print(task)
+    v1 = int(input())
+    v2 = int(input())
+    v3 = int(input())
+    v4 = int(input())
+    v5 = int(input())
+    if v1>v2 and v1>v3 and v1>v4 and v1>v5 :
+        print(f'{v1/v1:.2f}')
+        print(f'{v2/v1:.2f}')
+        print(f'{v3/v1:.2f}') 
+        print(f'{v4/v1:.2f}')
+        print(f'{v5/v1:.2f}')
+    elif v2>v1 and v2>v3 and v2>v4 and v2>v5 :
+        print(f'{v1/v2:.2f}')
+        print(f'{v2/v2:.2f}')
+        print(f'{v3/v2:.2f}')
+        print(f'{v4/v2:.2f}')
+        print(f'{v5/v2:.2f}')
+    elif v3>v1 and v3>v2 and v3>v4 and v3>v5 :
+        print(f'{v1/v3:.2f}')
+        print(f'{v2/v3:.2f}')
+        print(f'{v3/v3:.2f}')
+        print(f'{v4/v3:.2f}')
+        print(f'{v5/v3:.2f}')
+    elif v4>v1 and v4>v2 and v4>v3 and v4>v5 :
+        print(f'{v1/v4:.2f}')
+        print(f'{v2/v4:.2f}')
+        print(f'{v3/v4:.2f}')
+        print(f'{v4/v4:.2f}')
+        print(f'{v5/v4:.2f}')
+    elif v5>v1 and v5>v2 and v5>v3 and v5>v4 :
+        print(f'{v1/v5:.2f}')
+        print(f'{v2/v5:.2f}')
+        print(f'{v3/v5:.2f}')
+        print(f'{v4/v5:.2f}')
+        print(f'{v5/v5:.2f}')
 if __name__ == "__main__":
     norm()
